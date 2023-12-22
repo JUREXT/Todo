314e05cbf4fb8788d353c04ccb23265b1f5a1e94
Jure Lokovsek <lokovsek.jure@gmail.com>
2023-12-22 12:15:59 +0100

    Updated test for checking if fun getById returns http status OK.

--- src/test/kotlin/com/todo/TodoControllerTest.kt
+++ src/test/kotlin/com/todo/TodoControllerTest.kt
@@ -1,7 +1,9 @@
 package com.todo
 
+import com.todo.controller.TodoController
 import com.todo.entity.Todo
 import com.todo.repository.TodoRepository
+import io.micronaut.http.HttpStatus
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import jakarta.inject.Inject
 import org.junit.jupiter.api.Assertions
@@ -12,6 +14,10 @@ class TodoControllerTest {
 
     @Inject
     lateinit var repository: TodoRepository
+
+    @Inject
+    lateinit var todoController: TodoController
+
 
     @Test
     fun testCreateAndGetById() {
@@ -24,5 +30,24 @@ class TodoControllerTest {
         Assertions.assertEquals(savedTodo.id, retrievedTodo.get().id)
         Assertions.assertEquals(todo.name, retrievedTodo.get().name)
         Assertions.assertEquals(todo.description, retrievedTodo.get().description)
+    }
+
+    @Test
+    fun testGetByIdIsHttpStatusOK() {
+        // Given
+        val todo = Todo(
+            id = 1,
+            name = "Todo Name",
+            description = "Todo Desc",
+            tasks = emptyList()
+        )
+
+        repository.save(todo)
+
+        // When
+        val httpOKStatus = todoController.getById(id = 1)
+
+        // Then
+        Assertions.assertEquals(httpOKStatus.status, HttpStatus.OK)
     }
 }
